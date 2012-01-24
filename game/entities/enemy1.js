e8a43aa0d93ed2f227fd3b839d78b3cfd182d616
Paul Alexander Welch <paul@company100.net>
2012-01-24 18:55:22 +0900

    Fix ApplyRadialForce for Fluid
    Implementation had errors.

--- game/entities/enemy1.js
+++ game/entities/enemy1.js
@@ -49,7 +49,7 @@ function Enemy1(context, world, level, target, x, y, textureId) {
 
                     that.explodeY = (position.y / that.level.height) * fluidSolver.N;
 
-                    var explodeForceTween = new Tween(events, that, 'explosionForce', Tween.regularEaseIn, 200, 0, 0.05);
+                    var explodeForceTween = new Tween(events, that, 'explosionForce', Tween.regularEaseIn, 0, 400, 0.5);
 
                     var fadeOutTween = new Tween(events, that.vectorDraw3DComponent, 'alpha', Tween.regularEaseOut, 1, 0, 0.15);
 
