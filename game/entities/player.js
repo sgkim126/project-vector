31fa05a992d10d1c85fe3f04f5706450466a1d63
Paul Alexander Welch <paul@company100.net>
2012-02-05 19:55:54 +0900

    Update Player Sprite

--- game/entities/player.js
+++ game/entities/player.js
@@ -11,19 +11,29 @@ function Player(context, world, level, x, y, textureId) {
 
     this.weaponVector = new box2d.Vec2(1 * this.weaponPower, 0);
 
-    var model = models.octahedron;
-
     this.bodyComponent = new CircleBody(this, world, level, x, y, 12, 1);
+
     this.cameraFollowComponent = new CameraFollow(this, this.bodyComponent);
+
     this.controlMoveComponent = new ControlMove(this, this.bodyComponent);
-    this.vectorDraw3DComponent = new VectorDraw3D(this, model, this.bodyComponent, '#FFFF00', 0.75);
+
     this.weaponComponent = new Weapon(this, world, level, this.bodyComponent, this.weaponVector);
 
+    this.basicSprite = new BasicSprite(this, this.bodyComponent, 'player');
+
+    this.basicSprite.scaleModify = 0.7;
+
+    this.basicSprite.overrideRotation = true;
+
     this.components.add(context, this.bodyComponent, 'body');
+
     this.components.add(context, this.cameraFollowComponent, 'camera');
+
     this.components.add(context, this.controlMoveComponent, 'control');
+
     this.components.add(context, this.weaponComponent, 'weapon');
-    this.components.add(context, this.vectorDraw3DComponent, 'sprite');
+
+    this.components.add(context, this.basicSprite, 'sprite');
 
 };
 
@@ -35,14 +45,18 @@ Player.prototype = {
 
         var events = context.events;
 
-        var fadeInTween = new Tween(events, this.vectorDraw3DComponent, 'alpha', Tween.regularEaseIn, 0, 1, 0.25);
+        var fadeInTween = new Tween(events, this.basicSprite, 'alpha', Tween.regularEaseIn, 0, 1, 0.25);
 
         fadeInTween.start();
 
         this.lastX = 0;
+
         this.lastY = 0;
+
         this.x = 0;
+
         this.y = 0;
+
     },
 
     onUpdate: function (context) {
@@ -54,11 +68,15 @@ Player.prototype = {
         var body = this.bodyComponent.object;
 
             this.lastX = this.x;
+
             this.lastY = this.y;
+
             this.x = body.m_position.x;
+
             this.y = body.m_position.y;
 
             var dx = this.x - this.lastX;
+
             var dy = this.y - this.lastY;
 
             var d = Math.sqrt(dx * dx + dy * dy);
@@ -66,14 +84,21 @@ Player.prototype = {
             if (d > 0.00001) {
 
                 dx /= d;
+
                 dy /= d;
 
                 this.weaponVector.x = -dx * this.weaponPower;
+
                 this.weaponVector.y = -dy * this.weaponPower;
+
+                var rotation = Math.atan2(dy, dx);
+
+                this.basicSprite.rotation = rotation + (Math.PI / 2);
 
             }
 
         }
+
     },
 
     onRender: function (context) {
